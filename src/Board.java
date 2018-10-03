d6b14e7fff1b2ffac914e3897d2660dc0df89ce6
Tyler Spreen <tspree01@users.noreply.github.com>
2018-10-03 14:48:27 -0500

    making board

--- src/Board.java
+++ src/Board.java
@@ -1,2 +1,25 @@
 public class Board {
+	private final int ROWS = 3;
+	private final int COLOUMS = 3;
+	int[][] squares = new int[ROWS][COLOUMS];
+
+	Board(){
+
+	}
+
+	public void printToConsole() {
+
+		System.out.println("Board: ");
+
+		for (int i = 0; i < ROWS; ++ i) {
+			for (int j = 0; j < COLOUMS; j++) {
+
+				System.out.print(this.squares[i][j]);
+				System.out.print( " | ");
+			}
+			System.out.println();
+
+			System.out.println( "---+---+---");
+		}
+	}
 }

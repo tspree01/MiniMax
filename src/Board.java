51eb7ddbd918b3ffaed5f8f8ff0ab1750f57ed94
Tyler Spreen <tspree01@users.noreply.github.com>
2018-10-09 22:12:15 -0500

    Its working but the AI is an idiot

--- src/Board.java
+++ src/Board.java
@@ -1,39 +1,48 @@
+import javax.print.DocFlavor;
+import javax.swing.*;
 import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Vector;
 
 public class Board
 {
-	private final int Rows = 3;
-	private final int Columns = 3;
-	//ArrayList<Cell[]> squares = new ArrayList<>();
-	Cell[] cells = new Cell[9];
-	Board prev;
-	int miniMaxScore = 0;
+	final String playerAI = "O";
+	final String playerHuman = "X";
+	//List<Integer> scores = new ArrayList<>();
+	Cell[] cells;
 
 
 	Board()
 	{
+		this.cells = new Cell[9];
+		for (int i = 0; i < cells.length; i++)
+		{
+			cells[i] = new Cell(String.valueOf(i + 1), true);
+		}
 	}
 
-	Board(int Rows, int Column)
-	{
-		//squares = new String[Rows][Columns];
-	}
 
 	//copy constructor
-	public Board(Board _prev)
+	public Board(Board board)
 	{
-		this.prev = _prev;
-		System.arraycopy(_prev.cells, 0, this.cells, 0, _prev.cells.length);
+		this.cells = new Cell[9];
+		for (int i = 0; i < this.cells.length; i++)
+		{
+			this.cells[i] = new Cell(String.valueOf(board.cells[i].numberOnBoard),board.cells[i].spaceIsEmpty);
+		}
 	}
 
 	// place an X or an O where the current player selected
-	void move(int userInput, String currentPlayer)
+	void move(int userInput, String currentPlayer, Cell[] board)
 	{
-		this.cells[userInput - 1].numberOnBoard = currentPlayer;
-		this.cells[userInput - 1].spaceIsEmpty = false;
+			board[userInput - 1].numberOnBoard = currentPlayer;
+			board[userInput - 1].spaceIsEmpty = false;
+
 	}
 
-	ArrayList<Cell> availableCells(Cell[]board){
+	ArrayList<Cell> numberOfAvailableCells(Cell[] board)
+	{
 		ArrayList<Cell> emptyCells = new ArrayList<>();
 
 		for (Cell cell : board)
@@ -47,61 +56,141 @@ public class Board
 	}
 
 	//set all the cells to blank
-	void setupBoard()
+	void setupBoard(Cell[] board)
 	{
-		for (int i = 0; i < cells.length; i++)
-		{
-			cells[i] = new Cell(String.valueOf(i + 1), true);
-		}
+		board[0].numberOnBoard = "O";
+		board[0].spaceIsEmpty = false;
+		board[1].numberOnBoard = "X";
+		board[1].spaceIsEmpty = false;
+		board[2].numberOnBoard = "3";
+		board[3].numberOnBoard = "4";
+		board[4].numberOnBoard = "5";
+		board[5].numberOnBoard = "X";
+		board[5].spaceIsEmpty = false;
+		board[6].numberOnBoard = "O";
+		board[6].spaceIsEmpty = false;
+		board[7].numberOnBoard = "O";
+		board[7].spaceIsEmpty = false;
+		board[8].numberOnBoard = "X";
+		board[8].spaceIsEmpty = false;
+
+
 	}
 
-	// Check the number of cells that are empty
-	private int numberOfAvailableCells(Cell[] board)
+/*	// Check the number of cells that are empty
+	private ArrayList<Integer> numberOfAvailableCells(Cell[] board)
 	{
-		int numberOfAvailableSpaces = 0;
+		ArrayList<Integer> numberOfEmptyCells = new ArrayList<>();
+		int numberOnBoard = 1;
 		for (Cell cell : board)
 		{
-			if (cell.spaceIsEmpty)
-			{
-				numberOfAvailableSpaces++;
+			if(cell.spaceIsEmpty){
+				numberOfEmptyCells.add(numberOnBoard += 1);
 			}
 		}
-		return numberOfAvailableSpaces;
-	}
+		return numberOfEmptyCells;
+	}*/
 
-	int minimax(int depth, Board board, String currentPlayer)
+
+	int[] minimax(int depth, Board board, String currentPlayer)
 	{
-		//ArrayList<Integer> scores = new ArrayList<>();
+		ArrayList<Cell> availableMoves = this.numberOfAvailableCells(board.cells);
 		// Check to see if its a tie, win, or lose
 		// Win
 		if (this.aiHasWon(board.cells))
 		{
-			return 10 - depth;
+			return new int[]{10 - depth, 387565234};
 		}
 		// Lose
-		if (this.playerHasWon(board.cells))
+		else if (this.playerHasWon(board.cells))
 		{
-			return depth - 10;
+
+			return new int[]{depth - 10, 35234};
 		}
 		// Tie
-		if(numberOfAvailableCells(board.cells) == 0){
-			return 0;
-		}
-
-
-		for (int i = 0; i < board.cells.length; i++)
+		else if (availableMoves.isEmpty())
 		{
-			if(board.cells[i].spaceIsEmpty){
-				Board newBoard = new Board(board);
-				newBoard.move(i,currentPlayer);
-
-			}
-
+			return new int[]{0, 3875234};
 		}
 
+		ArrayList<int[]> scores = new ArrayList<>();
+		for (int i = 0; i < availableMoves.size(); i++)
+		{
+			// check to see if its the AI's turn
+			if (currentPlayer.equals("O"))
+			{
+				Board newBoard = new Board(board);
+				if (newBoard.cells[Integer.parseInt(availableMoves.get(i).numberOnBoard)- 1].spaceIsEmpty)
+				{
+					newBoard.move(Integer.parseInt(availableMoves.get(i).numberOnBoard), currentPlayer,newBoard.cells);
+
+					scores.add(minimax(depth += 1, newBoard, playerHuman));
+				}
+			}
+			else if (currentPlayer.equals("X"))
+			{
+				Board newBoard = new Board(board);
+				if (newBoard.cells[Integer.parseInt(availableMoves.get(i).numberOnBoard) - 1].spaceIsEmpty)
+				{
+					newBoard.move(Integer.parseInt(availableMoves.get(i).numberOnBoard), currentPlayer,newBoard.cells);
+					scores.add(minimax(depth += 1, newBoard, playerAI));
+				}
+			}
+		}
+
+		if (currentPlayer.equals("O"))
+		{
+			int maxScore = Integer.MIN_VALUE;
+			int maxIndex = 0;
+			for (int i = 0; i < scores.size(); i++)
+			{
+				if(scores.get(i)[0] > maxScore){
+					maxScore = scores.get(i)[0];
+					maxIndex = Integer.parseInt(availableMoves.get(i).numberOnBoard);;
+				}
+			}
+			return new int[]{maxScore, maxIndex};
+		}
+		else
+		{
+			int minScore = Integer.MAX_VALUE;
+			int minIndex = 0;
+			for (int i = 0; i < scores.size(); i++)
+			{
+				if(scores.get(i)[0] < minScore){
+					minScore = scores.get(i)[0];
+					minIndex = Integer.parseInt(availableMoves.get(i).numberOnBoard);
+				}
+			}
+			return new int[]{minScore, minIndex};
+		}
 	}
 
+	/*	class MaxComparator implements Comparator<Integer>
+		{
+			public int compare(Integer a, Integer b) {
+				if (a > b) {
+					return - 1;
+				}
+				if (a < b ) {
+					return 1;
+				}
+				return 0;
+			}
+		}
 
+		class MinComparator implements Comparator<Integer>
+		{
+			public int compare(Integer a, Integer b) {
+				if (a < b) {
+					return 1;
+				}
+				if (a > b ) {
+					return -1;
+				}
+				return 0;
+			}
+		}*/
 	// Check to see if the AI has won
 	private boolean aiHasWon(Cell[] board)
 	{
@@ -189,7 +278,7 @@ public class Board
 	}
 
 
-	void printBoard()
+	void printBoard(Cell[] board)
 	{
 		System.out.print(" " + cells[0].numberOnBoard + "  | ");
 		System.out.print(" " + cells[1].numberOnBoard + "  | ");
@@ -205,7 +294,6 @@ public class Board
 		System.out.print(" " + cells[7].numberOnBoard + "  | ");
 		System.out.print(" " + cells[8].numberOnBoard);
 		System.out.println();
-		System.out.println("Your Move: ");
 	}
 }
 
--- src/Game.java
+++ src/Game.java
@@ -10,6 +10,10 @@ public class Game
 	final String playerHuman = "X";
 	int userInput;
 	Board board;
+	Board actuallyBoard;
+	boolean computerHasWon = false;
+	boolean draw = false;
+	boolean playerHasWon = false;
 
 	public static void main(String[] args)
 	{
@@ -18,14 +22,184 @@ public class Game
 
 	void run()
 	{
+		int[] bestMove;
 		board = new Board();
-		board.setupBoard();
-		for (int k = 0; k < 9; k++)
+		//board.setupBoard(board.cells);
+		//actuallyBoard = new Board();
+
+		while (true)
 		{
-			board.printBoard();
-			//Get the user's move
-			userInput = sc.nextInt();
-			board.move(userInput,playerHuman);
+			if(!currentPlayerWon(board.cells))
+			{
+				//board.printBoard(board.cells);
+				board.printBoard(board.cells);
+				System.out.print("Your Move: ");
+				//Get the user's move
+					userInput = sc.nextInt();
+					board.move(userInput, playerHuman, board.cells);
+					if(!currentPlayerWon(board.cells))
+					{
+							bestMove = board.minimax(0, board, playerAI);
+							// best move[1] isnt getting the cell that bestmove[1] points too like index 1 can be cell 3 of available moves
+							board.move(bestMove[1], playerAI, board.cells);
+							System.out.println();
+					}
+
+				//board.move(userInput, playerHuman, actuallyBoard.cells);
+			}
+			else {
+				board.printBoard(board.cells);
+				if (computerHasWon){
+					System.out.println("AI Won");
+				}
+				else if(playerHasWon){
+					System.out.println("Player Won");
+				}
+				else {
+					System.out.println("Draw");
+				}
+				break;
+			}
+
+		}
+	}
+
+	// Check to see if the AI has won
+	private boolean currentPlayerWon(Cell[] board)
+	{
+		// Top Across
+		if (board[0].numberOnBoard.equals("O") && board[1].numberOnBoard.equals("O") && board[2].numberOnBoard.equals("O"))
+		{
+			computerHasWon = true;
+			playerHasWon = false;
+			draw = false;
+			return true;
+		}
+		//Middle Across
+		else if (board[3].numberOnBoard.equals("O") && board[4].numberOnBoard.equals("O") && board[5].numberOnBoard.equals("O"))
+		{
+			computerHasWon = true;
+			playerHasWon = false;
+			draw = false;
+			return true;
+		}
+		// Bottom Across
+		else if (board[6].numberOnBoard.equals("O") && board[7].numberOnBoard.equals("O") && board[8].numberOnBoard.equals("O"))
+		{
+			computerHasWon = true;
+			playerHasWon = false;
+			draw = false;
+			return true;
+		}
+		//Middle Down
+		else if (board[1].numberOnBoard.equals("O") && board[4].numberOnBoard.equals("O") && board[7].numberOnBoard.equals("O"))
+		{
+			computerHasWon = true;
+			playerHasWon = false;
+			draw = false;
+			return true;
+		}
+		// Right Down
+		else if (board[0].numberOnBoard.equals("O") && board[3].numberOnBoard.equals("O") && board[6].numberOnBoard.equals("O"))
+		{
+			computerHasWon = true;
+			playerHasWon = false;
+			draw = false;
+			return true;
+		}
+		// Left Down
+		else if (board[2].numberOnBoard.equals("O") && board[5].numberOnBoard.equals("O") && board[8].numberOnBoard.equals("O"))
+		{
+			computerHasWon = true;
+			playerHasWon = false;
+			draw = false;
+			return true;
+		}
+		//Top Left to Bottom Right
+		else if (board[0].numberOnBoard.equals("O") && board[4].numberOnBoard.equals("O") && board[8].numberOnBoard.equals("O"))
+		{
+			computerHasWon = true;
+			playerHasWon = false;
+			draw = false;
+			return true;
+		}
+		// Top Right to Bottom Left
+		else if (board[2].numberOnBoard.equals("O") && board[4].numberOnBoard.equals("O") && board[6].numberOnBoard.equals("O"))
+		{
+			computerHasWon = true;
+			playerHasWon = false;
+			draw = false;
+			return true;
+		}
+		// Top Across
+		else if (board[0].numberOnBoard.equals("X") && board[1].numberOnBoard.equals("X") && board[2].numberOnBoard.equals("X"))
+		{
+			playerHasWon = true;
+			computerHasWon = false;
+			draw = false;
+			return true;
+		}
+		//Middle Across
+		else if (board[3].numberOnBoard.equals("X") && board[4].numberOnBoard.equals("X") && board[5].numberOnBoard.equals("X"))
+		{
+			playerHasWon = true;
+			computerHasWon = false;
+			draw = false;
+			return true;
+		}
+		// Bottom Across
+		else if (board[6].numberOnBoard.equals("X") && board[7].numberOnBoard.equals("X") && board[8].numberOnBoard.equals("X"))
+		{
+			playerHasWon = true;
+			computerHasWon = false;
+			draw = false;
+			return true;
+		}
+		//Middle Down
+		else if (board[1].numberOnBoard.equals("X") && board[4].numberOnBoard.equals("X") && board[7].numberOnBoard.equals("X"))
+		{
+			playerHasWon = true;
+			computerHasWon = false;
+			draw = false;
+			return true;
+		}
+		// Right Down
+		else if (board[0].numberOnBoard.equals("X") && board[3].numberOnBoard.equals("X") && board[6].numberOnBoard.equals("X"))
+		{
+			playerHasWon = true;
+			computerHasWon = false;
+			draw = false;
+			return true;
+		}
+		// Left Down
+		else if (board[2].numberOnBoard.equals("X") && board[5].numberOnBoard.equals("X") && board[8].numberOnBoard.equals("X"))
+		{
+			playerHasWon = true;
+			computerHasWon = false;
+			draw = false;
+			return true;
+		}
+		//Top Left to Bottom Right
+		else if (board[0].numberOnBoard.equals("X") && board[4].numberOnBoard.equals("X") && board[8].numberOnBoard.equals("X"))
+		{
+			playerHasWon = true;
+			computerHasWon = false;
+			draw = false;
+			return true;
+		}
+		// Top Right to Bottom Left
+		else if(board[2].numberOnBoard.equals("X") && board[4].numberOnBoard.equals("X") && board[6].numberOnBoard.equals("X"))
+		{
+			playerHasWon = true;
+			computerHasWon = false;
+			draw = false;
+			return true;
+		}
+		else {
+			draw = true;
+			computerHasWon = false;
+			playerHasWon = false;
+			return false;
 		}
 	}
 }
